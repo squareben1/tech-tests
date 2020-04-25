e397336a2a55ecc3130b793d633edf9d29b3009f
Squareben1 <bengittins@live.com>
2020-04-25 22:13:50 +0100

    rename

---
+++ palindrome/maxDiff-25.04.20/lib/test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class Test
+  def maxDifference(arr)
+    max_diff = arr[1] - arr[0]
+
+    arr.each do |num|
+      index = arr.find_index(num)
+      arr[0..index].each do |i|
+        if (arr[index] - i) > max_diff
+          max_diff = arr[index] - i
+        end
+      end
+    end
+    max_diff
+  end
+
+  def breakPalindrome(str)
+    arr = str.split('')
+
+    arr.each do |char|
+      if char != 'a'
+        arr[arr.find_index(char)] = 'a'
+        break
+      end
+    end
+    uniq_check(arr)
+  end
+
+  def uniq_check(arr)
+    if arr.uniq.length == 1
+      'IMPOSSIBLE'
+    else
+      arr.join('')
+    end
+  end
+end
+
+
